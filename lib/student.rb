2d41b831a6bac93be04dc652cfccfa6558c61b26
Randy Steele <steelerx155@gmail.com>
2019-12-16 19:07:17 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -42,8 +42,8 @@ class Student
   end
   
   def self.new_from_db(row)
-    new_student = self.new
-    new_student.id = row[0]
+   pat = self.new
+    pat.id = row[0]
     # new_student.name = row[1]
     # new_student.length = row[2]
     new_student
