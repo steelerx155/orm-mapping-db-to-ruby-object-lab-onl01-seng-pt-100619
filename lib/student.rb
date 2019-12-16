e5933a1aa8e0bc82e2d91f793fba0089b9f93af3
Randy Steele <steelerx155@gmail.com>
2019-12-16 20:17:02 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -70,6 +70,13 @@ class Student
   end
   
   def self.first_student_in_grade_10
+   sql = <<-SQL
+   SELECT *
+   FROM students
+   WHERE grade = 10 
+   SQL 
+   end
+end
   
   def save
     sql = <<-SQL
