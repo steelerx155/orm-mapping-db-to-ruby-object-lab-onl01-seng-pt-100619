95f94d249457228f7aaefe7b6f713c50210a510d
Randy Steele <steelerx155@gmail.com>
2019-12-16 19:45:51 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -42,7 +42,7 @@ class Student
     sql = <<-SQL
     SELECT *
     FROM students
-    WHERE grade > 12
+    WHERE grade < 12
     SQL
    DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
