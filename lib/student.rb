46f2cc443336541a20da92eeaf9d8514228b70a9
Randy Steele <steelerx155@gmail.com>
2019-12-16 20:30:39 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -85,7 +85,8 @@ class Student
     sql = <<-SQL
     SELECT *
     FROM students
-    
+    WHERE grade = ?
+    ORDER BY students.id LIMIT 1
     SQL
     DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
