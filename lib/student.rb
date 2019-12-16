aafd320e50e62a932e892f7b2f841c854f0a3049
Randy Steele <steelerx155@gmail.com>
2019-12-16 19:15:02 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -42,7 +42,6 @@ class Student
   end
   
    def self.new_from_db(row)
-    # create a new Student object given a row from the database
     student = self.new
     student.id = row[0]
     student.name = row[1]
